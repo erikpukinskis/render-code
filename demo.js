a5dea0ca98f380aaa3745f56ed0efe810a5b0a7c
Erik Pukinskis <erik@snowedin.net>
2018-06-07 21:45:13 -0700

    pull a nice demo renderer out of write-code

---
+++ demo.js
@@ -0,0 +1,74 @@
+var library = require("module-library")(require)
+
+library.using(
+  [".", "web-element", "web-site", "browser-bridge", "basic-styles"],
+  function(renderCode, element, WebSite, BrowserBridge, basicStyles) {
+    var baseBridge = new BrowserBridge()
+    var site = new WebSite()
+    basicStyles.addTo(baseBridge)
+    var page = element(
+      "hi")
+
+    var webPage = [
+      "library.using([",
+      "  \"web-element\",",
+      "  \"web-site\",",
+      "  \"browser-bridge\",",
+      "  \"basic-styles\"],",
+      "  function(element, WebSite, BrowserBridge, basicStyles) {",
+      "    var baseBridge = new BrowserBridge()",
+      "    var site = new WebSite()",
+      "    basicStyles.addTo(",
+      "      baseBridge)",
+      "    var page = element(",
+      "      \"button\",",
+      "      \"Press Me\")",
+      "    site.addRoute(",
+      "      \"get\",",
+      "      \"/\",",
+      "      baseBridge.requestHandler(",
+      "        page))",
+      "    site.start(PORT)})",
+    ]
+
+    var activity = [
+      "orMaybe(",
+      "  just,",
+      "  \"a bit of poetry\")",
+      "    *",
+      "    ezjs",
+    ]
+
+    var baseBridge = new BrowserBridge()
+
+    var site = new WebSite()
+
+    site.addRoute(
+      "get",
+      "/",
+      function(request, response) {
+        var bridge = baseBridge.forResponse(response)
+
+        var left = bridge.partial()
+        var right = bridge.partial()
+        renderCode(left, webPage)
+        renderCode(right, activity)
+
+        left = element(element.style({
+          "margin-right": "100px",
+        }), left)
+
+        var page = element(
+          element.style({
+            "display": "flex",
+          }),[
+          left,
+          right])
+
+        bridge.send(page)
+      }
+    )
+    site.start(9919)
+  })
+
+
